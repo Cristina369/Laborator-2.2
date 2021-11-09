d9dd90db3265c01e356b7dea128bfac345fe7562
Cristina369 <93976122+Cristina369@users.noreply.github.com>
2021-11-09 13:07:05 +0200

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,12 +1,12 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-function on_device_orientation(evt){var alpha = evt.alpha;
-  var beta = evt.beta;var gamma = evt.gamma;
+function on_device_orientation(evt){let alpha = evt.alpha;
+  let beta = evt.beta;let gamma = evt.gamma;
   document.getElementById("a").innerHTML = "alpha = "+alpha;
   document.getElementById("b").innerHTML = "beta = "+beta;
   document.getElementById("c").innerHTML = "gamma = "+gamma;
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  var centru = {x:canvas.width/2, y:canvas.height/2}var latura_patrat = 50;
+  let canvas = document.getElementById("canvas");
+  let context = canvas.getContext("2d");
+  let centru = {x:canvas.width/2, y:canvas.height/2}let latura_patrat = 50;
 context.clearRect(0, 0, canvas.width, canvas.height);
 context.fillStyle = "#6A0888";
 context.beginPath();
